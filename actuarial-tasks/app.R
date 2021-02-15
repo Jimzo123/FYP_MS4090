2ca84a8fb316df21df48a31378e9edd52cd04184
conorlucey243 <conor.lucey@gmail.com>
2021-02-15 13:56:10 +0000

    matched Tommy&Grace's ofrmat + added risk profiler stuff

--- actuarial-tasks/app.R
+++ actuarial-tasks/app.R
@@ -40,7 +40,10 @@ ui <- dashboardPage(
         sidebarMenu(
             menuItem("Loan Calculator", tabName = "loan_calc"),
             menuItem("SORP Calculator", tabName = "sorp"),
-            menuItem("Drawdown Simulator", tabName = "drawdown")
+            menuItem("Drawdown Simulator", tabName = "drawdown"),
+            # menuItem("SORP & Drawdown", tabName = "sorp_x_drawdown"),
+            menuItem("SORP & Drawdown", tabName = "sorp_bh"),
+            menuItem("Risk Profiler", tabName = "riskprofiler")
         )
     ),
     
@@ -50,7 +53,10 @@ ui <- dashboardPage(
             tabItems(
                 tabItem(tabName = 'loan_calc', source("source_scripts/loan_calc_ui.R", local = TRUE)[1]),
                 tabItem(tabName = 'sorp', source("source_scripts/sorp_ui.R", local = TRUE)[1]),
-                tabItem(tabName = 'drawdown', source("source_scripts/drawdown_ui.R", local = TRUE)[1])
+                tabItem(tabName = 'drawdown', source("source_scripts/drawdown_ui.R", local = TRUE)[1]),
+                # tabItem(tabName = 'sorp_x_drawdown', source("source_scripts/sorp_x_drawdown_ui.R", local = TRUE)[1]),
+                tabItem(tabName = 'sorp_bh', source("source_scripts/sorp_bh_ui.R", local = TRUE)[1]),
+                tabItem(tabName = 'riskprofiler', source("source_scripts/risk_profiler_ui.R", local = TRUE)[1])
             )
         )
     )
@@ -58,9 +64,13 @@ ui <- dashboardPage(
 
 # General Server ----------------------------------------------------------
 server <- function(input, output, session) {
+    source("source_scripts/functions.R", local = TRUE)[1]
     source("source_scripts/loan_calc_server.R", local = TRUE)[1]
     source("source_scripts/sorp_server.R", local = TRUE)[1]
     source("source_scripts/drawdown_server.R", local = TRUE)[1]
+    # source("source_scripts/sorp_x_drawdown_server.R", local = TRUE)[1]
+    source("source_scripts/sorp_bh_server.R", local = TRUE)[1]
+    source("source_scripts/risk_profiler_server.R", local = TRUE)[1]
 }
 
 # Run the application 
